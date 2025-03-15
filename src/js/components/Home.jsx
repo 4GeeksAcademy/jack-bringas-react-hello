5db3cc02aef671a9f4ba1368801c6fa1f811dbd0
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-03-15 05:26:54 +0000

    final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,60 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import Card from "./Card.jsx";
+import Footer from "./Footer.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const data = [
+    {
+      cardTitle: "Playa Paraíso",
+      description: "Arenas blancas y aguas cristalinas te esperan en este rincón del mundo.",
+      image: "https://picsum.photos/id/215/500/325"
+    },
+    {
+      cardTitle: "Montañas Místicas",
+      description: "Un lugar perfecto para todos los amantes del senderismo y la naturaleza.",
+      image: "https://picsum.photos/id/229/500/325"
+    },
+    {
+      cardTitle: "Ciudad Histórica",
+      description: "Calles empedradas y edificios coloniales llenos de historias fascinantes.",
+      image: "https://picsum.photos/id/276/500/325"
+    },
+    {
+      cardTitle: "Selva Encantada",
+      description: "Descubre tanto la flora como la fauna exótica en este maravilloso ecosistema.",
+      image: "https://picsum.photos/id/666/500/325"
+    }
+  ];
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Navbar />
+      <Jumbotron />
+
+      <div className="container mt-5">
+        <div className="row g-4 justify-content-center">
+          {data.map((info, index) => {
+            return (
+              <div
+                className={`col-12 col-sm-6 col-md-4 col-lg-3 mb-4`} // Responsividad y espaciado entre columnas
+                key={index}
+              >
+                <Card
+                  cardTitle={info.cardTitle}
+                  description={info.description}
+                  image={info.image}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="container bg-light p-5 rounded">
+      <h1 className="display-4">¡Bienvenido!</h1>
+      <p className="lead">Nos complace que hayas llegado hasta aquí, te invitamos a explorar nuestro sitio.</p>
+    </div>
+  );
+};
+
+export default Jumbotron;
